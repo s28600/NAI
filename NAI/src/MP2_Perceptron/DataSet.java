663d283ccec0fc40d81b19944a285299ac04f496
s28600 <s28600@pjwstk.edu.pl>
2024-04-08 15:49:52 +0200

    Created DataSet class

---
+++ NAI/src/MP2_Perceptron/DataSet.java
@@ -0,0 +1,33 @@
+package MP2_Perceptron;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DataSet {
+    private List<Item> items = new ArrayList<>();
+    private static double[] range = new double[2];
+
+    public DataSet(String filepath) throws IOException {
+        Scanner scanner = new Scanner(Paths.get(filepath));
+        List<Double> params;
+        String[] line;
+        while (scanner.hasNextLine()){
+            line = scanner.nextLine().split(";");
+            params = new ArrayList<>();
+            for (int i = 0; i < line.length-1; i++) {
+                double param = Double.parseDouble(line[i]);
+                params.add(param);
+                if (param < range[0]) range[0] = param;
+                if (param > range[1]) range[1] = param;
+            }
+            items.add(new Item(params, line[line.length-1]));
+        }
+    }
+
+    public List<Item> getItems(){return items;}
+    public double getMin(){return range[0];}
+    public double getMax(){return range[1];}
+}
--- NAI/src/MP2_Perceptron/Perceptron.java
+++ NAI/src/MP2_Perceptron/Perceptron.java
@@ -4,27 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Perceptron {
-    double[] range;
     double a;
-    List<Item> trainSet;
+    List<Item> items;
     double[] weights;
 
-    public Perceptron(double[] range, double a, List<Item> trainSet) {
-        this.range = range;
+    public Perceptron(double a, DataSet trainSet) {
         this.a = a;
-        this.trainSet = trainSet;
-        weights = new double[trainSet.getFirst().params.size()];
+        this.items = trainSet.getItems();
+        weights = new double[trainSet.getItems().getFirst().params.size()];
 
         for (int i = 0; i < weights.length; i++) {
-            weights[i] = Math.random()*range[1]*2-range[1];
+            weights[i] = Math.random()*(trainSet.getMax()-trainSet.getMin())+trainSet.getMin();
         }
     }
 
     @Override
     public String toString() {
         return "Perceptron{" +
-                "range=" + Arrays.toString(range) +
-                ", a=" + a +
+                "a=" + a +
                 ", weights=" + Arrays.toString(weights) +
                 '}';
     }
