e5c101ad448ca79302205976a2be8276657b7d13
s28600 <s28600@pjwstk.edu.pl>
2024-04-15 16:57:07 +0200

    Few changes to perceptron

---
+++ SingleLayerNeuralNetwork/src/Perceptron.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class Perceptron {
+    double[] weights;
+    double bias;
+
+    public Perceptron(double[] weights, double bias) {
+        this.weights = weights;
+        this.bias = bias;
+    }
+
+    public double net(double[] params){
+        if (params.length != weights.length){
+            throw new RuntimeException("Input vector has different length than " +
+                    "perceptron weights vector - unable to compute.");
+        }
+        double net = -bias;
+        for (int i = 0; i < params.length; i++) {
+            net += params[i]*weights[i];
+        }
+        return net;
+    }
+
+    public double y(double[] params){
+        return net(params)>=0?1:0;
+    }
+
+    public void learn(double[] params, int d, int y, double a){
+        if (params.length != weights.length){
+            throw new RuntimeException("Input vector has different length than " +
+                    "perceptron weights vector - unable to learn.");
+        }
+        for (int i = 0; i < params.length; i++) {
+            weights[i] += (d-y)*a*params[i];
+        }
+        bias -= (1-y)*a;
+    }
+
+    @Override
+    public String toString() {
+        return "Perceptron:" +
+                "\n\tWeights: " + Arrays.toString(weights) +
+                "\n\tBias: " + bias;
+    }
+}
