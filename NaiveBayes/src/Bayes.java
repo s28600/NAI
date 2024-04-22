135838cde3b697b08638b1b4f056bd1a5c51fa31
s28600 <s28600@pjwstk.edu.pl>
2024-04-22 21:29:11 +0200

    Logic done

--- NaiveBayes/src/Bayes.java
+++ NaiveBayes/src/Bayes.java
@@ -1,8 +1,7 @@
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class Bayes {
+    static final boolean DEBUG = false;
     List<String[]> trainSet;
     HashMap<Integer, List<String>> availableAttributes;
     HashMap<String, Integer> decisiveAttributesCount;
@@ -13,7 +12,7 @@ public class Bayes {
         decisiveAttributesCount = new HashMap<>();
 
         for (String[] line : trainSet){
-
+            //Collecting available attributes
             for (int i = 0; i < line.length-1; i++) {
                 if (!availableAttributes.containsKey(i)){
                     availableAttributes.put(i, new ArrayList<>());
@@ -23,23 +22,66 @@ public class Bayes {
                 }
             }
 
+            //Collecting decisive attributes
             if (!decisiveAttributesCount.containsKey(line[line.length-1]))
                 decisiveAttributesCount.put(line[line.length-1], 0);
             decisiveAttributesCount.replace(line[line.length-1], decisiveAttributesCount.get(line[line.length-1])+1);
         }
+
+        System.out.println(availableAttributes);
+        System.out.println(decisiveAttributesCount);
     }
 
-    public int conditionalProbability(String[] attributes, String condition){
+    public double conditionalProbability(String[] attributes, String condition){
+        //User error checking
         if (attributes.length != availableAttributes.size())
             throw new RuntimeException("Attributes count does not match available attributes");
-
+        else if (!decisiveAttributesCount.containsKey(condition))
+            throw new RuntimeException("Condition not present in available decisive attributes");
         for (int i = 0; i < attributes.length; i++) {
             if (!availableAttributes.get(i).contains(attributes[i]))
                 throw new RuntimeException("Attribute number " + (i+1) + " is not present in available attributes");
         }
 
+        int[] attributesCount = new int[attributes.length];
+        for (String[] line : trainSet){
+            if (!line[line.length-1].equals(condition)) continue;
 
+            for (int i = 0; i < line.length-1; i++) {
+                if (attributes[i].equals(line[i])) attributesCount[i]++;
+            }
+        }
 
-        return 0;
+        double probability = 1;
+        for (int i = 0; i < attributesCount.length; i++) {
+            if (attributesCount[i] == 0){
+                probability *= (double) (attributesCount[i]+1) / (decisiveAttributesCount.get(condition)+availableAttributes.get(i).size());
+            } else {
+                probability *= (double) attributesCount[i] / decisiveAttributesCount.get(condition);
+            }
+        }
+        probability *= (double) decisiveAttributesCount.get(condition)/trainSet.size();
+
+        //System.out.println(Arrays.toString(attributes));
+        //System.out.println(Arrays.toString(attributesCount));
+
+        return probability;
+    }
+
+    public String getVerdict(String[] attributes){
+        String[] verdicts = decisiveAttributesCount.keySet().toArray(new String[0]);
+        double[] verdictsProbabilities = new double[decisiveAttributesCount.size()];
+        for (int i = 0; i < verdicts.length; i++) {
+            verdictsProbabilities[i] = conditionalProbability(attributes, verdicts[i]);
+        }
+        int index = 0;
+        double max = 0;
+        for (int i = 0; i < verdictsProbabilities.length; i++) {
+            if (verdictsProbabilities[i] > max){
+                max = verdictsProbabilities[i];
+                index = i;
+            }
+        }
+        return verdicts[index];
     }
 }
--- NaiveBayes/src/Main.java
+++ NaiveBayes/src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Bayes bayes = new Bayes(Util.readFile("data/trainingset.csv"));
-        System.out.println(bayes.availableAttributes);
-        System.out.println(bayes.decisiveAttributesCount);
-        System.out.println(bayes.conditionalProbability(new String[]{"deszcz","tak","niska","niska"}, "tak"));
+        String[] attributes = {"snieg","tak","wysoka","srednia"};
+        System.out.println(bayes.conditionalProbability(attributes, "tak"));
+        System.out.println(bayes.conditionalProbability(attributes, "nie"));
+        System.out.println(bayes.getVerdict(attributes));
+        System.out.println();
+        for (String[] line : bayes.trainSet){
+            String expected = line[line.length-1];
+            String actual = bayes.getVerdict(new String[]{line[0],line[1],line[2],line[3]});
+            System.out.println("Expected: " + expected + ", actual: " + actual + " " + (!expected.equals(actual)?"FAIL":""));
+        }
     }
 }
