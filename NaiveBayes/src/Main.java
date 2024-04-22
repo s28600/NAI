3cdfce18ec95f37e4c3447af7143025a5fae6b1f
s28600 <s28600@pjwstk.edu.pl>
2024-04-22 22:35:56 +0200

    Everything other than manual enter implemented

--- NaiveBayes/src/Main.java
+++ NaiveBayes/src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Bayes.DEBUG = true;
         Bayes bayes = new Bayes(Util.readFile("data/trainingset.csv"));
-        String[] attributes = {"snieg","tak","wysoka","srednia"};
-        System.out.println(bayes.getVerdict(attributes));
-        System.out.println();
-        bayes.checkTrainSet();
-        bayes.checkTestSet(Util.readFile("data/testset.csv"));
+        UI ui = new UI(bayes);
+        ui.start();
     }
 }
--- NaiveBayes/src/UI.java
+++ NaiveBayes/src/UI.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class UI {
+    static Scanner scanner = new Scanner(System.in);
+    private static final String[] optionsMenu = {
+            "\nPlease select option:",
+            "Check for train set",
+            "Check for test set",
+            "Toggle debug mode",
+            "Enter manually",
+            "Quit"};
     Bayes bayes;
 
     public UI(Bayes bayes) {
@@ -6,6 +16,35 @@ public class UI {
     }
 
     public void start(){
+        while (true) {
+            int option = readOption();
+            System.out.println();
+            switch (option){
+                case 1 -> bayes.checkTrainSet();
+                case 2 -> bayes.checkTestSet(Util.readFile("data/testset.csv"));
+                case 3 -> Bayes.DEBUG=!Bayes.DEBUG;
+                case 4 -> System.out.println();
+                case 5 -> {
+                    scanner.close();
+                    return;
+                }
+            }
+        }
+    }
 
+    private static int readOption(){
+        System.out.println(optionsMenu[0]);
+        for (int i = 1; i < optionsMenu.length; i++) {
+            System.out.println("(" + i + ") " + optionsMenu[i]);
+        }
+        while (true) {
+            try {
+                System.out.print("-> ");
+                int option = Integer.parseInt(scanner.next());
+                if (option >= 1 && option <= optionsMenu.length-1) {
+                    return option;
+                }
+            } catch (NumberFormatException ignored) {}
+        }
     }
 }
